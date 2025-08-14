d2c9744b6dd432eb1157081483f4170c8c8a0d10
Ahmed <Ahmed@DESKTOP-PLMR0LC>
2025-08-14 12:25:20 +0300

    Rendering category table from localstorage.

---
+++ assets/js/seedCategories.js
@@ -0,0 +1,49 @@
+function seedProducts() {
+  let existingCategories = JSON.parse(localStorage.getItem("categories")) || [];
+  if (existingCategories.length > 0) return;
+
+  console.log(existingCategories);
+
+  const categories = [
+    {
+      id: Date.now(),
+      name: "Phones",
+      description: "Smartphones and mobile accessories.",
+      status: "active",
+    },
+    {
+      id: Date.now() + 1,
+      name: "Smart Watch",
+      description: "Wearable smart devices and fitness trackers.",
+      status: "active",
+    },
+    {
+      id: Date.now() + 2,
+      name: "Cameras",
+      description: "Digital cameras, lenses, and photography accessories.",
+      status: "active",
+    },
+    {
+      id: Date.now() + 3,
+      name: "Headphones",
+      description: "Wired and wireless headphones and earphones.",
+      status: "active",
+    },
+    {
+      id: Date.now() + 4,
+      name: "Computers",
+      description: "Laptops, desktops, and computer accessories.",
+      status: "active",
+    },
+    {
+      id: Date.now() + 5,
+      name: "Gaming",
+      description: "Gaming consoles, controllers, and accessories.",
+      status: "active",
+    },
+  ];
+
+  localStorage.setItem("categories", JSON.stringify(categories));
+}
+
+window.addEventListener("load", seedProducts);
