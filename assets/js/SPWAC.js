5e767472393a67146dc0a41381e5af73ffe03e86
ahmedtalat-s <ahmedtalaatz102@gmail.com>
2025-08-16 16:35:27 +0300

    add wishlist and cart btn funcitionality

--- assets/js/SPWAC.js
+++ assets/js/SPWAC.js
@@ -1,1 +1,82 @@
-window.addEventListener("load", () => {});
+//declare variable
+const productId = new URLSearchParams(location.search).get("id");
+const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+
+let userss = JSON.parse(localStorage.getItem("users"));
+
+window.addEventListener("load", () => {
+  // query elenents
+  let whishlistBtn = document.getElementById("wishListBtn"),
+    cartBtn = document.getElementById("cartBtn");
+
+  // get user from local storage
+  function getCurrentUser() {
+    return userss.find((u) => u.email === currentUser?.email);
+  }
+
+  //get all users
+  function getUsers() {
+    userss = JSON.parse(localStorage.getItem("users"));
+  }
+  //save user
+  function saveUsers() {
+    localStorage.setItem("users", JSON.stringify(userss));
+  }
+
+  // event listener
+  // event for whislist
+  whishlistBtn.addEventListener("click", function () {
+    const user = getCurrentUser();
+    if (!user) {
+      window.location.href = "../../pages/auth/login.html";
+      return;
+    }
+
+    user.favorites = user.favorites || [];
+
+    if (user.favorites.includes(productId)) {
+      // user.favorites = user.favorites.filter((id) => id !== productId);
+      showToast("The Item Already exist in wishlist", "warning");
+    } else {
+      showToast("Item has been Added", "success");
+      user.favorites.push(productId);
+    }
+
+    saveUsers();
+  });
+  cartBtn.addEventListener("click", function () {
+    console.log("here");
+
+    const user = getCurrentUser();
+    if (!user) {
+      window.location.href = "../../pages/auth/login.html";
+      return;
+    }
+
+    user.cart = user.cart || [];
+    const existingItem = user.cart.find((item) => item.productId === productId);
+    if (existingItem) {
+      existingItem.quantity += Number(1);
+      showToast(
+        "The Item Already exist in Cart and quantity was incremented by one",
+        "success"
+      );
+    } else {
+      user.cart.push({ productId, quantity: 1 });
+      showToast("The Item has been Added Cart", "success");
+    }
+
+    saveUsers();
+  });
+});
+
+function showToast(message, type = "danger") {
+  let toastEl = document.getElementById("toastMessage");
+
+  toastEl.className = `toast align-items-center text-bg-${type} border-0`;
+
+  toastEl.querySelector(".toast-body").textContent = message;
+
+  let toast = new bootstrap.Toast(toastEl);
+  toast.show();
+}
